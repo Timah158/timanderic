20f5c8a45dce83ee9fdd9e344f3e4e697891d775
Eric Moore <emoore36@my.gcu.edu>
2022-09-09 21:25:38 -0700

    Replace # hrefs with /

--- src/navbar.js
+++ src/navbar.js
@@ -7,8 +7,8 @@ function Navbar() {
       <nav className="navbar">
         <a href="/" className="logo_link"><img src={logo} className="App-logo" alt="logo" /></a>
         <ul className="navlist">
-          <li className="navlink"><a className="navlink" href="#">Tim</a></li>
-          <li className="navlink"><a className="navlink" href="#">Eric</a></li>
+          <li className="navlink"><a className="navlink" href="/">Tim</a></li>
+          <li className="navlink"><a className="navlink" href="/">Eric</a></li>
           <li className="navlink"><a className="navlink" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Interview</a></li>
         </ul>
       </nav>
