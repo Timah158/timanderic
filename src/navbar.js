ebe565697d865a999873e7e22d67cee01b2dd3de
Timah158 <66193811+Timah158@users.noreply.github.com>
2022-10-05 01:17:31 -0500

    Updated Navbar for mobile.

--- src/navbar.js
+++ src/navbar.js
@@ -1,11 +1,15 @@
 import {ReactComponent as Logo} from './SVGs/logo.svg'
+import {ReactComponent as MobileLogo} from './SVGs/Mobile logo.svg'
+import {ReactComponent as MenuIcon} from './SVGs/menu.svg'
 import './CSS/navbar.css';
 
 function Navbar() {
     return (
       <header>
         <nav className="navbar">
-        <a href="/"><Logo className='navbar_logo'/></a>
+        <a href="/" className='mobile_navbar_logo_link mobile'><MobileLogo className='mobile_navbar_logo'/></a>
+        <a href="/" className='navbar_logo_link'><Logo className='navbar_logo'/></a>
+        <button className='menu_icon mobile'><MenuIcon/></button>
           <ul className="navlist">
             <li className="navlink"><a className="navlink" href="/Tim">Tim</a></li>
             <li className="navlink"><a className="navlink" href="/Eric">Eric</a></li>
