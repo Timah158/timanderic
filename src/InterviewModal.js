4754a7a71b3d48a7f94543cb355c6c1b04ee5067
Tim Lopez <timothylopez95@gmail.com>
2023-01-07 00:32:36 -0600

    Added CSS to interview modal.

--- src/InterviewModal.js
+++ src/InterviewModal.js
@@ -5,18 +5,20 @@ function InterviewModal() {
     return (
       <div className='modal_wrapper'>
         <div className='modal_content'>
-            <CloseIcon/>
+            <CloseIcon className='close_button'/>
             <h3>Schedule An Interview</h3>
-            <form>
-                <input type="date"/>
-                <input type="time"/>
-                <select name="person" id="person">
-                  <option value="Tim">Tim</option>
-                  <option value="Eric">Eric</option>
-                </select>
-                <input type="email"/>
-                <input type="tel"/>
-                <textarea></textarea>
+            <form id='interview_form'>
+                <div id='interview_row1'>
+                  <input type="date"/>
+                  <input type="time"/>
+                  <select name="person" id="person">
+                    <option value="Tim">Tim</option>
+                    <option value="Eric">Eric</option>
+                  </select>
+                  <input type="email" placeholder='email@email.com'/>
+                  <input type="tel" placeholder='(888)-888-8888'/>
+                </div>
+                <textarea placeholder='Tell us about your company...'></textarea>
                 <input type="submit"/>
             </form>
         </div>
