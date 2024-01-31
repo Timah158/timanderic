28c93a5e4383ef0b843273a6ee05282f4c4f2701
Timah158 <timothylopez95@gmail.com>
2024-01-30 21:50:47 -0600

    Removed amplify.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import EricProfile from "./EricProfile";
 import InterviewModal from "./InterviewModal";
 import ProjectModal from "./ProjectModal";
 import Footer from "./footer";
-import { Amplify } from 'aws-amplify';
-import awsmobile from './aws-exports';
+// import { Amplify } from 'aws-amplify';
+// import awsmobile from './aws-exports';
 
-Amplify.configure(awsmobile);
+// Amplify.configure(awsmobile);
 
 function App() {
 
