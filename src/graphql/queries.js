ca126a11233dfa7cffd0d9ff3bd18f8bfa09911d
Timah158 <timothylopez95@gmail.com>
2024-02-01 14:22:38 -0600

    updated amplify

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,6 +1,124 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const getProjectSection = /* GraphQL */ `
+  query GetProjectSection($id: ID!) {
+    getProjectSection(id: $id) {
+      id
+      title
+      images
+      text
+      links
+      index
+      projectID
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listProjectSections = /* GraphQL */ `
+  query ListProjectSections(
+    $filter: ModelProjectSectionFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listProjectSections(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        images
+        text
+        links
+        index
+        projectID
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const projectSectionsByProjectID = /* GraphQL */ `
+  query ProjectSectionsByProjectID(
+    $projectID: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelProjectSectionFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    projectSectionsByProjectID(
+      projectID: $projectID
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        title
+        images
+        text
+        links
+        index
+        projectID
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getProject = /* GraphQL */ `
+  query GetProject($id: ID!) {
+    getProject(id: $id) {
+      id
+      title
+      createdOn
+      completedOn
+      description
+      createdBy
+      hidden
+      status
+      ProjectSections {
+        nextToken
+        __typename
+      }
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listProjects = /* GraphQL */ `
+  query ListProjects(
+    $filter: ModelProjectFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listProjects(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        createdOn
+        completedOn
+        description
+        createdBy
+        hidden
+        status
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
 export const getInterview = /* GraphQL */ `
   query GetInterview($id: ID!) {
     getInterview(id: $id) {
