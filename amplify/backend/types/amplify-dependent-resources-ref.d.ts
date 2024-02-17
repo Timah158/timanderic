222fe5a13e9b23e5de47fde4efb0ea6fd8716271
Timah158 <timothylopez95@gmail.com>
2024-02-16 22:20:13 -0600

    Removed Amplify Backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "timanderic": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
